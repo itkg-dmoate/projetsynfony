f0e756fcd53fe1c433ce92a4f8c06668626758d1
d.moate <david.moate@gmail.com>
2013-12-21 14:31:05 +0100

    ceci est un test

--- src/David/BlogBundle/Controller/BlogController.php
+++ src/David/BlogBundle/Controller/BlogController.php
@@ -9,7 +9,7 @@ class BlogController extends Controller
 {
   public function indexAction()
   {
-      echo 'branch';
+      echo 'tata';
       $articles = array(
       array(
         'titre'   => 'Mon weekend a Phi Phi Island !',
