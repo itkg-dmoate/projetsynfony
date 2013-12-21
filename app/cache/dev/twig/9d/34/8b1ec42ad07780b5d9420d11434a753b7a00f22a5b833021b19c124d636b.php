f654c9c1c4127f63332ddda5bce488193248cbd4
unknown <david.moate@gmail.com>
2013-12-21 11:55:53 +0100

    first commit

---
+++ app/cache/dev/twig/9d/34/8b1ec42ad07780b5d9420d11434a753b7a00f22a5b833021b19c124d636b.php
@@ -0,0 +1,57 @@
+<?php
+
+/* DavidBlogBundle:Blog:menu.html.twig */
+class __TwigTemplate_9d348b1ec42ad07780b5d9420d11434a753b7a00f22a5b833021b19c124d636b extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 2
+        echo "
+<h3>Les derniers articles</h3>
+
+<ul class=\"nav nav-pills nav-stacked\">
+  ";
+        // line 6
+        $context['_parent'] = (array) $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["liste_articles"]) ? $context["liste_articles"] : $this->getContext($context, "liste_articles")));
+        foreach ($context['_seq'] as $context["_key"] => $context["article"]) {
+            // line 7
+            echo "    <li><a href=\"";
+            echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("david_blog_voir", array("id" => $this->getAttribute((isset($context["article"]) ? $context["article"] : $this->getContext($context, "article")), "id"))), "html", null, true);
+            echo "\">";
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["article"]) ? $context["article"] : $this->getContext($context, "article")), "titre"), "html", null, true);
+            echo "</a></li>
+  ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['article'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 9
+        echo "</ul>";
+    }
+
+    public function getTemplateName()
+    {
+        return "DavidBlogBundle:Blog:menu.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  40 => 9,  29 => 7,  25 => 6,  19 => 2,);
+    }
+}
---
+++ src/David/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace David\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class BlogController extends Controller
+{
+  public function indexAction()
+  {
+      $articles = array(
+      array(
+        'titre'   => 'Mon weekend a Phi Phi Island !',
+        'id'      => 1,
+        'auteur'  => 'winzou',
+        'contenu' => 'Ce weekend était trop bien. Blabla…',
+        'date'    => new \Datetime()),
+      array(
+        'titre'   => 'Repetition du National Day de Singapour',
+        'id'      => 2,
+        'auteur' => 'winzou',
+        'contenu' => 'Bientôt prêt pour le jour J. Blabla…',
+        'date'    => new \Datetime()),
+      array(
+        'titre'   => 'Chiffre d\'affaire en hausse',
+        'id'      => 3, 
+        'auteur' => 'M@teo21',
+        'contenu' => '+500% sur 1 an, fabuleux. Blabla…',
+        'date'    => new \Datetime())
+    );
+      
+      return $this->render('DavidBlogBundle:Blog:index.html.twig', array(
+          'articles' => $articles
+        ));
+  }
+  
+public function voirAction($id)
+{
+    $articles = array(
+      array(
+        'titre'   => 'Mon weekend a Phi Phi Island !',
+        'id'      => 1,
+        'auteur'  => 'winzou',
+        'contenu' => 'Ce weekend était trop bien. Blabla…',
+        'date'    => new \Datetime()),
+      array(
+        'titre'   => 'Repetition du National Day de Singapour',
+        'id'      => 2,
+        'auteur' => 'winzou',
+        'contenu' => 'Bientôt prêt pour le jour J. Blabla…',
+        'date'    => new \Datetime()),
+      array(
+        'titre'   => 'Chiffre d\'affaire en hausse',
+        'id'      => 3, 
+        'auteur' => 'M@teo21',
+        'contenu' => '+500% sur 1 an, fabuleux. Blabla…',
+        'date'    => new \Datetime())
+    );
+    $articleOk    = array();
+    foreach( $articles as $article){
+        if($article['id'] == $id){
+            $articleOk    = $article;
+            break;
+        }        
+    }
+  // Puis modifiez la ligne du render comme ceci, pour prendre en compte l'article :
+  return $this->render('DavidBlogBundle:Blog:voir.html.twig', array(
+    'article' => $articleOk
+  ));
+}
+  
+  public function menuAction($nombre) // Ici, nouvel argument $nombre, on l'a transmis via le render() depuis la vue
+  {
+    $liste = array(
+      array('id' => 2, 'titre' => 'Mon dernier weekend !'),
+      array('id' => 5, 'titre' => 'Sortie de Symfony2.1'),
+      array('id' => 9, 'titre' => 'Petit test')
+    );
+    $liste  =   array_slice($liste, 0, $nombre);
+    return $this->render('DavidBlogBundle:Blog:menu.html.twig', array(
+      'liste_articles' => $liste // C'est ici tout l'intérêt : le contrôleur passe les variables nécessaires au template !
+    ));
+  }
+}
