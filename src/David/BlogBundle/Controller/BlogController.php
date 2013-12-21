37e59ebd8076a6d859ad8db6cf43c98f2b9ca742
d.moate <david.moate@gmail.com>
2013-12-21 15:10:20 +0100

    tata test projet2

--- src/David/BlogBundle/Controller/BlogController.php
+++ src/David/BlogBundle/Controller/BlogController.php
@@ -10,6 +10,7 @@ class BlogController extends Controller
   public function indexAction()
   {
       echo 'tata test projet';
+      echo'tata test projet2';
       $articles = array(
       array(
         'titre'   => 'Mon weekend a Phi Phi Island !',
